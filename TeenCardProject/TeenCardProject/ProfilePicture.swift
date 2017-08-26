fbf77217813bd63dc2edefa08bd53ff1b0059319
Clara Carneiro <claracarneiro@172-13-0-92.lightspeed.mssnks.sbcglobal.net>
2017-08-26 18:52:08 -0300

    Confirm

--- TeenCardProject/TeenCardProject/ProfilePicture.swift
+++ TeenCardProject/TeenCardProject/ProfilePicture.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ProfilePicture: UIImageView {
 
+
     
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
