dfbe2b2a8699a734745a6a746ae507fb702bc717
Marcelo Martimiano Junior <mark3@172-13-0-87.lightspeed.mssnks.sbcglobal.net>
2017-08-27 03:13:32 -0300

    TeenCard class

--- TeenCardProject/TeenCardProject/GerenciadorDeCartoes.swift
+++ TeenCardProject/TeenCardProject/GerenciadorDeCartoes.swift
@@ -16,10 +16,10 @@ class GerenciadorDeCartoes
     static func getCartoesDoCliente() -> [CartaoPrePago]
     {
          self.meusCartoes = [CartaoPrePago]()
-        meusCartoes.append(CartaoPrePago(proxy: "3713100018790", numeroDoCartao: "4213040011977072", nomeCompleto: "Vitor Schiavo", tipo: .TeenCardFilho, validade: "03/20"))
+        meusCartoes.append(CartaoPrePago(proxy: "3713100018790", numeroDoCartao: "4213040011977072", nomeCompleto: "Vitor Schiavo", tipo: .TeenCardDependente, validade: "03/20"))
         meusCartoes.append(CartaoPrePago(proxy: "3713100018808", numeroDoCartao: "4213040011938918", nomeCompleto: "marcelo Silva", tipo: .MaisQuePresente, validade: "03/20"))
-        meusCartoes.append(CartaoPrePago(proxy: "3713100018816", numeroDoCartao: "4213040011952828", nomeCompleto: "lucas Silva", tipo: .TeenCardFilho, validade: "03/20"))
-        meusCartoes.append(CartaoPrePago(proxy: "3713100018824", numeroDoCartao: "4213040011964575", nomeCompleto: "filipe silva", tipo: .TeenCardFilho, validade: "03/20"))
+        meusCartoes.append(CartaoPrePago(proxy: "3713100018816", numeroDoCartao: "4213040011952828", nomeCompleto: "lucas Silva", tipo: .TeenCardTitular, validade: "03/20"))
+        meusCartoes.append(CartaoPrePago(proxy: "3713100018824", numeroDoCartao: "4213040011964575", nomeCompleto: "filipe silva", tipo: .TeenCardDependente, validade: "03/20"))
         meusCartoes.append(CartaoPrePago(proxy: "3713100018832", numeroDoCartao: "4213040011990091", nomeCompleto: "clara silva", tipo: .MaisQuePresente, validade: "03/20"))
         
         return meusCartoes
