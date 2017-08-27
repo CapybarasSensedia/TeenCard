9e5ed0a972b5b242d365c65f82690e0f71eab9de
Marcelo Martimiano Junior <mark3@172-13-0-87.lightspeed.mssnks.sbcglobal.net>
2017-08-26 22:51:00 -0300

    bla

--- TeenCardProject/TeenCardProject/GerenciadorDeCartoes.swift
+++ TeenCardProject/TeenCardProject/GerenciadorDeCartoes.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class GerenciadorDeCartoes
 {
+    static var cartaoAtual: CartaoPrePago?
+    static var meusCartoes = [CartaoPrePago]()
+    
     static func getCartoesDoCliente() -> [CartaoPrePago]
     {
-        var meusCartoes = [CartaoPrePago]()
+        self.meusCartoes = [CartaoPrePago]()
         meusCartoes.append(CartaoPrePago(proxy: "3713100018790", numeroDoCartao: "4213040011977072", nomeCompleto: "Vitor Schiavo", tipo: .TeenCardFilho))
         meusCartoes.append(CartaoPrePago(proxy: "3713100018808", numeroDoCartao: "4213040011938918", nomeCompleto: "marcelo Silva", tipo: .MaisQuePresente))
         meusCartoes.append(CartaoPrePago(proxy: "3713100018816", numeroDoCartao: "4213040011952828", nomeCompleto: "lucas Silva", tipo: .TeenCardFilho))
@@ -20,5 +23,10 @@ class GerenciadorDeCartoes
         meusCartoes.append(CartaoPrePago(proxy: "3713100018832", numeroDoCartao: "4213040011990091", nomeCompleto: "clara silva", tipo: .MaisQuePresente))
         
         return meusCartoes
+    }
+    
+    static func setCartaoAtual()
+    {
+        self.cartaoAtual = self.meusCartoes[0]
     }
 }
--- TeenCardProject/TeenCardProject/MinhaContaViewController.swift
+++ TeenCardProject/TeenCardProject/MinhaContaViewController.swift
@@ -9,34 +9,60 @@
 import UIKit
 
 class MinhaContaViewController: UIViewController {
-
+    
     @IBOutlet var recarregarButton: UIButton!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var cardNumberLabel: UILabel!
+    @IBOutlet weak var saldo: UILabel!
     
+    @IBOutlet weak var saldoActivityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.recarregarButton.layer.cornerRadius = 10
         self.recarregarButton.clipsToBounds = true
+        
         //self.recarregarButton.backgroundColor = UIColor.secondaryAppColor().withAlphaComponent(0.7)
+        self.carregaDados()
         
     }
-
+    func carregaDados()
+    {
+        self.atualizaSaldo()
+        
+        let numero = (GerenciadorDeCartoes.cartaoAtual?.numeroDoCartao)!
+        let last4 = numero.substring(from: numero.index(numero.endIndex, offsetBy: -4))
+        print(last4)
+        self.cardNumberLabel.text = "XXXX-XXXX-XXXX-" + last4
+    }
+    
+    func atualizaSaldo()
+    {
+        self.saldoActivityIndicator.startAnimating()
+        AgilitaOperacoesDeCredito.getSaldo
+            { (saldo, erro) in
+                self.saldoActivityIndicator.stopAnimating()
+                if let meuSaldo = saldo
+                {
+                    self.saldo.text = "\(meuSaldo)"
+                }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
