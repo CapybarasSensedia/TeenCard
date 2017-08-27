1b928bbb17d68308d6be9dfd21f6f2cb7f6b273a
Clara Carneiro <claracarneiro@172-13-0-92.lightspeed.mssnks.sbcglobal.net>
2017-08-27 03:29:31 -0300

    Add Gamification views and Linha do Tempo

--- TeenCardProject/TeenCardProject/LinhaDoTempoViewController.swift
+++ TeenCardProject/TeenCardProject/LinhaDoTempoViewController.swift
@@ -8,17 +8,21 @@
 
 import UIKit
 
-class LinhaDoTempoViewController: UIViewController {
+class LinhaDoTempoViewController: UITableViewController {
 
+    
+    let iconImage = ["Acougue", "Pastinha", "Supermercado", "Card", "User"]
+    let infoText = ["Romulo Carnes R$65,50" , "Riachuelo R$150", "Amigão R$ 50,60", "Desbloqueado", "Com o aplicativo do Teen Card você poderá monitorar seus gastos, fazer transferência e poupar dinheiro"]
+    let labelText = ["Açougue", "Vestuário", "Supermercado", "Seu cartão agora está", "Monitoramento"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let nib = UINib(nibName: "LinhaDoTempoCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "LinhaDoTempoCell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    // status bar with white text color
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -26,5 +30,38 @@ class LinhaDoTempoViewController: UIViewController {
     }
 
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LinhaDoTempoCell", for: indexPath) as! LinhaDoTempoTableViewCell
+        cell.icon.image = UIImage(named: (iconImage[indexPath.item]+".png"))?.withRenderingMode(.alwaysTemplate)
+        cell.label.text = labelText[indexPath.item]
+        cell.info.text = infoText[indexPath.item]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120.0
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 375.0
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+
+    
 }
 
